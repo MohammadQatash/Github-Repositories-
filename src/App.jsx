63e5d18f6641aabb9cb3d866c0e93d5eac92b0ea
MohammadQatash <qatashm@yahoo.com>
2022-07-06 20:22:44 +0300

    adding functionalty to create repo properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchForm from "./components/SearchForm";
 import ReposeCards from "./components/ReposeCards";
-import { data } from './data';
+import Loading from "./components/Loading";
+import Error from "./components/Error";
+
+const url = "https://api.github.com/search/repositories?q=";
 
 const App = () => {
-  const [repose, setRepose] = useState(data);
+  const [loading, setLoading] = useState(false);
+  const [repose, setRepose] = useState([]);
+  const [err, setErr] = useState('');
+
+  const hi = function(e) {
+    e.preventDefault();
+    console.log('hi');
+  }
+
+  const fetchRepos = async (reposeName) => {
+    setLoading(true);
+    try {
+      const response = await fetch(url + `${reposeName}`);
+      const repose = await response.json();
+      setLoading(false);
+      setRepose(repose.items);
+    } catch (error) {
+      setLoading(false);
+      setErr(error);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRepos("50projects50days");
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (err) {
+    return (
+      <main>
+        <Error />
+      </main>
+    );
+  }
 
   return (
     <main className="main">
       <div className="container">
         <section className="repos-search">
-          <SearchForm />
+          <SearchForm hi={hi} />
         </section>
 
         <section className="repositories-cards">
-          <ReposeCards repose={repose} />
-          <ReposeCards repose={repose} />
-          <ReposeCards repose={repose} />
-          <ReposeCards repose={repose} />
+          {repose.map((repo, repoIndex) => {
+            return <ReposeCards key={repoIndex} {...repo} />;
+          })}
         </section>
       </div>
     </main>
